ec7473ef7f6ca2deb4bdbbf6c21060b7dbb2dde3
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2020-09-15 19:54:18 -0600

    Remove core-js and @babel/transform-runtime from lib code
    Also remove stage-4 syntax transforms covered by @babel/preset-env

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,21 +1,3 @@
-const BABEL_ENV = process.env.BABEL_ENV;
-const IS_TEST = BABEL_ENV === "test"
+const defaultConfig = require('../../babel.config')
 
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        modules: ["cjs", "test"].includes(BABEL_ENV) ? "commonjs" : false,
-        targets:
-          IS_TEST ? { node: "current" } : { browsers: "defaults" },
-      },
-    ],
-    "@babel/preset-react"
-  ],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-optional-chaining"
-  ],
-  ignore: IS_TEST ? [] : ['test/**/*.js'],
-};
+module.exports = defaultConfig
