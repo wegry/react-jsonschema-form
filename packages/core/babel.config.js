942d14c107652e14781e05365a86542ffd77d8ea
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2020-09-15 18:50:40 -0600

    Don't pull in all of core-js with core (#1601)

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,12 +1,5 @@
 const BABEL_ENV = process.env.BABEL_ENV;
-
-const defaultPlugins = [
-  '@babel/plugin-proposal-class-properties',
-  '@babel/plugin-proposal-object-rest-spread',
-  '@babel/plugin-proposal-optional-chaining',
-  // IE 11 support
-  '@babel/plugin-transform-object-assign'
-];
+const IS_TEST = BABEL_ENV === "test"
 
 module.exports = {
   presets: [
@@ -15,30 +8,14 @@ module.exports = {
       {
         modules: ["cjs", "test"].includes(BABEL_ENV) ? "commonjs" : false,
         targets:
-          BABEL_ENV === "test" ? { node: "current" } : { browsers: "defaults" },
+          IS_TEST ? { node: "current" } : { browsers: "defaults" },
       },
     ],
-    "@babel/preset-react",
+    "@babel/preset-react"
   ],
-  env: {
-    cjs: {
-      plugins: defaultPlugins,
-      ignore: ['test/**/*.js']
-    },
-    umd: {
-      plugins: defaultPlugins,
-      ignore: ['test/**/*.js']
-    },
-    es: {
-      plugins: [
-        ...defaultPlugins,
-        ['@babel/plugin-transform-runtime', { useESModules: true, corejs: 2 }]
-      ],
-      ignore: ['test/**/*.js']
-    },
-    test: {
-      plugins: defaultPlugins,
-      ignore: []
-    }
-  },
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-optional-chaining"
+  ],
+  ignore: IS_TEST ? [] : ['test/**/*.js'],
 };
